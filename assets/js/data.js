e02cef5628333f90ee1874d9416f04a965426e1f
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-06 20:36:43 +0300

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,31 @@
+const data = {
+    "blogName": "BLOG::DOVGAL D.F.",
+    "navMenu": {
+        "nameNavMenu": {
+            "homePage": "ГЛАВНАЯ",
+            "blogPage": "БЛОГ",
+            "aboutPage": "ОБО МНЕ" 
+        },
+        "linkNavMenu": {
+            "homePage": "./index.html",
+            "blogPage": "./blog.html",
+            "aboutPage": "./dovgaldima.pp.ua" 
+        }
+    },
+    "headerTitle": "БЛОГ О WEB - РАЗРАБОТКЕ",
+    "freshArticle": "СВЕЖИЕ СТАТЬИ",
+    "blogArticle": "БЛОГ",
+    "blogArticleDescription": `Здесь вы найдете самые распространенные вопросы и 
+                                ответы в мире веб-разработки, которые помогут вам оставаться на плаву. 
+                                Эти статьи покрывают все, от <b>JavaScript</b> до <b>PHP</b>, и представлены в ярком и интересном формате.`,
+    "cardsArticle": [
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 1", "descriptionCard": "1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 2", "descriptionCard": "2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 3", "descriptionCard": "3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 4", "descriptionCard": "4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 5", "descriptionCard": "5 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+        {"linkPageInCard": "blog-page-name-1.html", "linkImgCard": "./assets/img/code.jpg", "titleCard": "Title Desc 6", "descriptionCard": "6 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, repellendus?"},
+    ]
+    
+};
+
