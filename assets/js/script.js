ed26e744d3559c8ba51f18389346dd07b12730b3
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2023-05-06 19:01:02 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
+const mainContent = document.querySelector('#mainContent');
+const btnDown = document.querySelector('#btnDown');
 
+function handleButtonClick() {
+    mainContent.scrollIntoView({ block: "start", behavior: "smooth" });
+}
+
+btnDown.addEventListener('click', handleButtonClick);
